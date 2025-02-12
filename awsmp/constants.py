adb4729f007062732ff4901e9518c0b6e247566c
Jess Jang <jess.jang@canonical.com>
2025-02-12 17:20:16 -0600

    feat: Get full details of ami product
    Introduces a new function to retrieve
    full details of the AMI product,
    including public offer details.

--- awsmp/constants.py
+++ awsmp/constants.py
@@ -74,3 +74,11 @@ CATEGORIES = [
 
 # Offer field keys
 TYPE = "Type"
+TEXT = "Text"
+SUPPORT_TERM = "SupportTerm"
+REFUND_POLICY = "RefundPolicy"
+USAGE_BASED_PRICING_TERM = "UsageBasedPricingTerm"
+RATECARD = "RateCard"
+RATECARDS = "RateCards"
+URL = "Url"
+TERMS = "Terms"
